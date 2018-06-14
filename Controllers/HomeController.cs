05d5722663d4b1ce8c987966c9c91082bc93709c
uprun <geraclion@gmail.com>
2018-06-14 22:56:51 +0300

    changed how skills are retrieved based on latest version of node and reference to skill node.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -351,7 +351,6 @@ namespace Skills.Controllers
         }
         public IActionResult Index()
         {
-            
             return View();
         }
 
@@ -363,10 +362,58 @@ namespace Skills.Controllers
             using(var context = new SkillsContext())
             {
                 
-                skills = context.Nodes
+                var allNodes = context.Nodes
                     .Include(n => n.tags)
-                    .Where(n => n.tags.FirstOrDefault(t => t.tag == "type" && t.value == "skill") != null)
-                    .ToArray();
+                    .ToList();
+                
+                var nodesLookup = allNodes.ToDictionary(n => n.id);
+                allNodes.ForEach(n => {
+                    var previousTag = n.tags.FirstOrDefault(x => x.tag == "system-reference:previous");
+                    if(previousTag != null) 
+                    {
+                        long previousId;
+                        bool parsable = long.TryParse(previousTag.value, out previousId);
+                        if(parsable && nodesLookup.ContainsKey(previousId))
+                        {
+                            nodesLookup[previousId] = null;
+                        }
+                    }
+                });
+
+                var skillTypes = allNodes
+                    .Where(x => x.tags.Exists(t => t.tag == "type" && t.value == "instanceTemplate"))
+                    .Where(x => x.tags.Exists(t => t.tag == "%type" && t.value == "skill"))
+                    .ToDictionary(x => x.id);
+
+                skills = nodesLookup.Values.Where(n => {
+                    if(n == null)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        var typeTag = n.tags.FirstOrDefault(x => x.tag == "system-reference:type");
+                        if(typeTag != null) 
+                        {
+                            long typeId;
+                            bool parsable = long.TryParse(typeTag.value, out typeId);
+                            if(parsable && skillTypes.ContainsKey(typeId))
+                            {
+                                return true;
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                        }
+                        else
+                        {
+                            return false;
+                        }
+
+                    }
+                }).ToArray();
+                
             }
             
             return Json( skills );
